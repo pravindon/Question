29a9ebfa316a461158c04d188d437e85889e13bd
Pravin Dongardive <pdpravin47@gmail.com>
2020-03-28 23:24:28 +0530

    Question repo

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatRippleModule,
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatSelectModule,
+  MatExpansionModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatListModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatCardModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatIconModule
+} from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule
+  ]
+})
+export class LayoutModule {}
