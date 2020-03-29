af9f00a5f13512ba9d6203b71797547fd337a248
Pravin Dongardive <pdpravin47@gmail.com>
2020-03-29 09:35:41 +0530

    New module and comp with material work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JavascriptComponent } from './layout/javascript/javascript.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Question-Bank';
+  panelOpenState = false;
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,39 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-  MatButtonModule,
-  MatInputModule,
-  MatRippleModule,
-  MatFormFieldModule,
-  MatTooltipModule,
-  MatSelectModule,
-  MatExpansionModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatListModule,
-  MatCheckboxModule,
-  MatGridListModule,
-  MatCardModule,
-  MatChipsModule,
-  MatAutocompleteModule,
-  MatIconModule
-} from '@angular/material';
-
+import {  MatButtonModule } from '@angular/material/button';
+import {  MatInputModule } from '@angular/material/input';
+import {  MatRippleModule } from '@angular/material/core';
+import {  MatFormFieldModule} from '@angular/material/form-field';
+import {  MatToolbarModule } from '@angular/material/toolbar';
+import {  MatTooltipModule } from '@angular/material/tooltip';
+import {  MatSelectModule} from '@angular/material/select';
+import {  MatExpansionModule} from '@angular/material/expansion';
+import {  MatTabsModule } from '@angular/material/tabs';
+import {  MatListModule } from '@angular/material/list';
+import {  MatCheckboxModule } from '@angular/material/checkbox';
+import {  MatGridListModule } from '@angular/material/grid-list';
+import {  MatCardModule} from '@angular/material/card';
+import {  MatChipsModule } from '@angular/material/chips';
+import {  MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { JavascriptComponent } from './javascript/javascript.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [JavascriptComponent],
   imports: [
     CommonModule,
     MatButtonModule,
     MatInputModule,
     MatRippleModule,
     MatFormFieldModule,
+    MatToolbarModule,
     MatTooltipModule,
     MatSelectModule,
     MatExpansionModule,
     MatTabsModule,
-    MatToolbarModule,
     MatListModule,
     MatCheckboxModule,
     MatGridListModule,
@@ -41,6 +39,26 @@ import {
     MatChipsModule,
     MatAutocompleteModule,
     MatIconModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    JavascriptComponent
   ]
 })
 export class LayoutModule {}
